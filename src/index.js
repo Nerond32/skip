a24f74875006fe7c196720728b6bee2fb7c88889
Nerond32 <nerond32@gmail.com>
2019-02-12 23:35:51 +0100

    Webpack multiple entry js files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './bige_pige.jpg';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
-  
+    var btn = document.createElement('button');
+    btn.innerHtml = "click this shit REEEEEEEEEEEE";
+    btn.onclick = printMe;
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hellou', 'webpack'], ' ');
     element.classList.add('hello');
+    element.appendChild(btn);
     var myIcon = new Image();
     myIcon.src = Icon;
     return element;
