a539c60871bc40c716af37b289c3a40c2fe15dba
Nerond32 <nerond32@gmail.com>
2019-02-12 23:09:38 +0100

    Webpack image loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './bige_pige.jpg';
 
 function component() {
     let element = document.createElement('div');
@@ -7,7 +8,8 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hellou', 'webpack'], ' ');
     element.classList.add('hello');
-  
+    var myIcon = new Image();
+    myIcon.src = Icon;
     return element;
   }
   
