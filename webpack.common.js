30e23a19c6b52420517865ff49cf344eb8e941d6
ezkwikr <krzysztof.kwiecien@ericsson.com>
2019-02-13 16:06:13 +0100

    Dev and prod webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
-  mode: "none",
   entry: {
     app: "./src/index.js"
   },
