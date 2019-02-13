36cfeef12c9cf252e49a7e1212dce93f8638f3ac
ezkwikr <krzysztof.kwiecien@ericsson.com>
2019-02-13 13:10:36 +0100

    CleanWebpackPlugin(clearing dist)

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'none',
+  mode: "none",
   entry: {
-    app: './src/index.js', 
-    print: './src/print.js'
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Output Management'
-    })  
+      title: "Output Management"
+    }),
+    new CleanWebpackPlugin(["dist"])
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-          }
-      ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
   }
 };
