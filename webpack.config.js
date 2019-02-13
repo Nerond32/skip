ce112763136d1f6cc7576ee5abcf423a9061398a
ezkwikr <krzysztof.kwiecien@ericsson.com>
2019-02-13 14:22:44 +0100

    Webpack-dev-server with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "none",
   entry: {
-    app: "./src/index.js",
-    print: "./src/print.js"
+    app: "./src/index.js"
   },
   output: {
     filename: "[name].bundle.js",
@@ -16,11 +16,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Output Management"
     }),
-    new CleanWebpackPlugin(["dist"])
+    new CleanWebpackPlugin(["dist"]),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
+    hot: true
   },
   module: {
     rules: [
