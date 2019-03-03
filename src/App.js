588a3f51cc23d38ca79812d740e895773a1ff263
Nerond32 <nerond32@gmail.com>
2019-03-03 18:13:27 +0100

    ESLint and prettier set up and configured

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="hello">Hello world!</h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import "./style.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 
-const element = <h1>Hello world!</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
