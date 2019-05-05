a19e00d843b23651ece2e1c46d49b713d996b14c
Nerond32 <nerond32@gmail.com>
2019-05-05 13:13:27 +0200

    Detached store to separate file

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./components/App";
-import rootReducer from "./redux/rootReducer";
+import store from "./redux/store";
 import "normalize.css";
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 const Root = () => (
   <Provider store={store}>
