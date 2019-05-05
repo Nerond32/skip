824fed7a0c7f59db6b7935a30344f9d8c8e00fe9
Nerond32 <nerond32@gmail.com>
2019-05-05 10:31:24 +0200

    Login page embryo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,52 @@
 import React from "react";
 
-const Login = () => {
-  return (
-    <div>
-      <h2>Future login page</h2>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onChange(e) {
+    e.preventDefault();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              placeholder="Enter email"
+              name="email"
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="text"
+              placeholder="Enter password"
+              name="password"
+              required
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
